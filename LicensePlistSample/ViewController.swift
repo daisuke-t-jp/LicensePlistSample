3f12856b4ddfb34e80b1a977a275ccb4648eb8c5
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-08-23 13:46:18 +0900

    init

---
+++ LicensePlistSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LicensePlistSample
+//
+//  Created by Daisuke T on 2019/08/23.
+//  Copyright © 2019 DaisukeT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
